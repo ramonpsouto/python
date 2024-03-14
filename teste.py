9bc5a55eb7e926e0198eab93e9e2d09af18618c7
Ramon Souto <132109032+ramonpsouto@users.noreply.github.com>
2024-03-14 17:15:46 +0000

    initial commit

---
+++ teste.py
@@ -0,0 +1,9 @@
+a = 5
+b = 2
+
+x = a + b
+
+print('o resultado final é ', x)
+print('o resultado final é %d'%x) #exibe em decimal
+print('o resultado final é {}'.format(x))
+print(f'o resultado final é {x}')
